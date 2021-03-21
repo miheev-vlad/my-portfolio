342d28e488238650fe87ac3b9c408d000648d99a
Mikheev Vladislav <micheev_v_a@mail.ru>
2021-03-21 16:34:29 +0600

    deploy210320211634

--- src/components/Header.js
+++ src/components/Header.js
@@ -85,7 +85,7 @@ export default function Header() {
       </AppBar>
 
       <div className={classes.container}>
-        <Title size="220" text1="FULL STACK" text2="JS DEVELOPER" open={true} />
+        <Title size="210" text1="JS DEVELOPER" open={true} />
         <Collapse
           in={checked}
           {...(checked ? { timeout: 1000 } : {})}
